04e807b92fd93060bcfe5a88087ee5c391d657d8
Badalgupta123 <badalgupta7898@gmail.com>
2024-02-07 20:17:18 +0530

    user authentication done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './Login'
 import Browse from './Browse'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, useNavigate } from 'react-router-dom'
+import {  onAuthStateChanged } from "firebase/auth";
+import { auth } from '../utils/Firebase';
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
+
+
 
 const Body = () => {
+    const dispatch = useDispatch();
+    const navigate= useNavigate();
 
     const appRouter = createBrowserRouter([
         {
@@ -15,6 +23,29 @@ const Body = () => {
             element:<Browse/>
         }
     ]);
+
+    useEffect(()=>{
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/auth.user
+          // you can get many things from this user object
+          
+          const {uid,email,displayName,profileURL} = user;
+          dispatch(addUser({uid:uid, email:email, displayName:displayName, profileURL:profileURL}));
+          // as the user signed in show him browser page
+          //navigate('/browse')
+        } else {
+          // User is signed out
+          // ...
+          dispatch(removeUser());
+          // if user sign out naviagate him to home page
+          // navigate('/'); we cannot navigate from here as 
+          // routerprovider is parent and we can navigate from child of routerprovider only
+        }
+      }); 
+      
+    },[])
   return (
     <div>
        <RouterProvider router={appRouter}/>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,86 @@
-import React,{useState} from 'react'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import Header from './Header'
+import checkValidData from "../utils/Validate"
+import { useRef, useState } from 'react';
+import { auth } from "../utils/Firebase";
+import { useNavigate } from "react-router-dom";
+import {  updateProfile } from "firebase/auth";
+import { useDispatch } from "react-redux";
+
 
 const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const [isSignInForm ,setIsSignInForm] = useState(true)
+  const [errorMessage,setErrorMessage] = useState(null);
+  
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef(null);
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
+  }
+  
+  const handleButtonClick= () => {
+    // validation
+    //we will use email.current.value as our userref is pointing to the input field
+    // email.current is input field so to fetch the value of input field use .value
+
+    const message = checkValidData(email.current.value,password.current.value)
+    setErrorMessage(message);
+    
+    if(message) return ;
+
+    // if no error then signup or signin 
+    if(!isSignInForm){
+      // signup // take logic from firebase doc
+      createUserWithEmailAndPassword(auth, email.current.value,password.current.value)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        // as soon as user sign up update its profile with display name which he has given
+        updateProfile(auth.user, {
+          displayName: name.current.value, photoURL: "https://example.com/jane-q-user/profile.jpg"
+        }).then(() => {
+          // Profile updated!
+          // ...
+          const {uid,email,displayName,profileURL} = auth.currentUser;
+          dispatch(addUser({uid:uid, email:email, displayName:displayName, profileURL:profileURL}));
+          navigate("/browse")
+        }).catch((error) => {
+          // An error occurred
+          // ...
+          setErrorMessage(error.message);
+        });
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        setErrorMessage(errorCode+"-"+errorMessage)
+      });
+    }
+    else{
+      // signin // get this logic from firebase doc
+      signInWithEmailAndPassword(auth,email.current.value,password.current.value)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/browse")
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setErrorMessage(errorCode+ "-" +errorMessage)
+      });
+    }
+
+
   }
   return (
     <div>
@@ -13,30 +89,38 @@ const Login = () => {
 
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/4da5d2b1-1b22-498d-90c0-4d86701dffcc/98a1cb1e-5a1d-4b98-a46f-995272b632dd/IN-en-20240129-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="" />
         </div> 
-        <form className='p-12 opacity-80 text-white w-3/12 mx-auto  my-36 absolute bg-black right-0 left-0'>
+        <form onSubmit={(e)=> e.preventDefault()}
+          className='p-12 opacity-80 text-white w-3/12 mx-auto  my-36 absolute bg-black right-0 left-0'>
             <h1 className='text-3xl font-bold py-4'>
               { isSignInForm ? "Sign In":"Sign Up"}
             </h1>
            
            
            {!isSignInForm && <input 
+              ref={name}
               type='text'
               placeholder='Name'
               className='p-4 my-4 w-full bg-gray-700'
             />}
             <input 
+              ref={email}
               type='email'
               placeholder='Email Address'
               className='p-4 my-4 w-full bg-gray-700'
             /> 
             <input 
+              ref={password}
               type='password'
               placeholder='Password' 
               className='p-4 my-4 w-full bg-gray-700'
             />
-            <button className='bg-red-700 p-4 my-6 w-full rounded-lg'>
+            <p className='text-lg text-red-500 py-2 font-bold'>{errorMessage}</p>
+            <button className='bg-red-700 p-4 my-6 w-full rounded-lg'
+              onClick={handleButtonClick}
+            >
               { isSignInForm ? "Sign In":"Sign Up"}
             </button>
+
             <p className='py-4 cursor-pointer'
                onClick={toggleSignInForm}>
                 { isSignInForm ? "New to Netflix? Sign Up Now":"Already registered? Sign In Now"}
